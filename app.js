0d6fce8237df34f2a232d8cdf61aead0078a52d4
Michael Leung <michael.kb.leung@gmail.com>
2025-03-01 00:22:42 -0500

    Add javascript to toggle mobile hamburger menu

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Happy developing ✨')
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+// Display mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
