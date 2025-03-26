2c82f0a3e39370ca2abca8eaafac14d4f3e9a903
Michael Leung <michael.kb.leung@gmail.com>
2025-03-26 13:56:29 -0400

    Add navbar section highlight effect

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 // Display mobile menu
 const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;    // Track vertical scroll position on website
+
+    // adds 'highlight' class to menu items ONLY if on desktop (i.e. not mobile)
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        servicesMenu.classList.remove('highlight');
+        homeMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos > 1400) {
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
+
+    if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight');
+    }
 }
 
-menu.addEventListener('click', mobileMenu)
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', document.querySelector('#navbar__logo'));
